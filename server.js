ccdd0256bda1558f2d86462fadc3cd5acb72cc40
Tim Keller <timothy.m.keller@gmail.com>
2021-02-11 01:05:21 -0800

    Added assignment readme, service-worker.js, manifest.webmanifest, and all icons. Matched server.js to my own personal preferred formatting style.

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require( "express" );
+const logger = require( "morgan" );
+const mongoose = require( "mongoose" );
+const compression = require( "compression" );
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use( logger( "dev" ) );
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use( compression());
+app.use( express.urlencoded({ extended: true }));
+app.use( express.json());
 
-app.use(express.static("public"));
+app.use( express.static( "public" ));
 
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budgettracker", 
 {
@@ -24,8 +24,8 @@ mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budgettracker"
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use( require( "./routes/api.js" ));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen( PORT, () => {
+  console.log( `App running on port ${PORT}!` );
 });
